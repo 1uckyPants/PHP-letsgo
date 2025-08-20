ad2320094be40f88d65d57c570ef5a83634ad63a
1ucky_pants <vasa.7775555@gmail.com>
2025-08-20 13:05:23 +0500

    справлен .gitignore: исключены MySQL файлы

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -5,26 +5,48 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use PDO;
 
 class HomeController
 {
     public function index(): View
     {
+        try {
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+                PDO::ATTR_EMULATE_PREPARES => false
+            ]);
+
+            $email = 'Gio@doe.com';
+            $name = 'Gio Doe';
+            $isActive = 1;
+            $createdAt = date('Y-m-d H:m:i', strtotime('07/11/2021 9:00PM'));
+            $query = 'INSERT INTO users (email, full_name, is_active, created_at, updated_at)
+                      VALUES (:email , :name, :active, :date1, :date2)';
+
+
+            $stmt = $db->prepare($query);
+
+            $stmt->bindValue(':name', $name);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':active', $isActive, PDO::PARAM_BOOL);
+            $stmt->bindValue(':date1', $createdAt);
+            $stmt->bindValue(':date2', $createdAt);
+
+            $stmt->execute();
+
+            $id = (int) $db->lastInsertId();
+
+            $user = $db->query('SELECT * FROM users WHERE id = ' . $id)->fetch();
+
+                echo "<pre>";
+                var_dump($user);
+                echo "</pre>";
+
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+
         return View::make('index', ['foo' => 'bar']);
     }
 
-    public function upload()
-    {
-        echo '<pre>';
-        var_dump($_FILES);
-        echo '</pre>';
-
-        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
-
-        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
-
-        echo '<pre>';
-        var_dump(pathinfo($filePath));
-        echo '</pre>';
-    }
 }
